29eef1cdad3d02676656b3e0bc4b802652f0f30d
Amy <kamyllyconstantino@gmail.com>
2025-09-21 12:35:45 -0300

    Create style.css
    Simple css development

---
+++ style.css
@@ -0,0 +1,26 @@
+body {
+  font-family: Arial, sans-serif;
+  background-color: #f4f4f9;
+  padding: 20px;
+}
+
+h1 {
+  color: #333;
+}
+
+label {
+  display: block;
+  margin-top: 10px;
+}
+
+input {
+  margin-bottom: 10px;
+  padding: 5px;
+  width: 200px;
+}
+
+button {
+  margin-top: 10px;
+  padding: 6px 12px;
+  cursor: pointer;
+}
